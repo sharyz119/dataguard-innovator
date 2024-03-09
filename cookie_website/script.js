e3037fac520a474769ad45a991041f2e04b4edf2
Day1712 <117100082+Day1712@users.noreply.github.com>
2024-03-09 19:16:51 +0100

    Website design improvements
    I (Rhodé) improved the look of the website a bit but it can be better still

--- cookie_website/script.js
+++ cookie_website/script.js
@@ -1,4 +1,8 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // 实现登录逻辑
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
